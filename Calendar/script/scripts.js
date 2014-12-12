c101aed7f49c8223c20e6179fef6259fc52de3a8
AnnaSobchuk <noname.anna@gmail.com>
2014-12-12 14:51:00 +0200

    redesign task div

--- Calendar/script/scripts.js
+++ Calendar/script/scripts.js
@@ -5,7 +5,8 @@
  */
 
 //main.addEventListener("load",setScreenParam, false);
-//window.onresize=setScreenParam();
+//window.onresize=setScreenParam(); 
+//$(window).load(SetScreenParam);
 function SetScreenParam() {
 
     var width = document.body.clientWidth;
@@ -15,43 +16,95 @@ function SetScreenParam() {
     CreateTableBody(today.getMonth(), today.getFullYear());
     taskboard.style.width = (width - 310) + 'px';
     date.innerText = 'Today, ' + today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear();
-    button.addEventListener('click', AddTask, false);
+    $('#button').click(Task.addTask);
+
+    //button.addEventListener('click', Task.addTask, false);
     //dayBegin.addEventListener('onfocus', ClearData(this), false);
 }
 
 //
 function ClearData(obj) {
-  obj.value = ' ';
+    obj.value = ' ';
 
 }
+var Task = (function () {
+    var div;
+    return {
+        addTask: function () {
+            div = document.createElement('div');
+            var root = document.getElementById('taskboard');
+            var begin = parseInt(document.getElementById('dayBegin').value);
+            var end = parseInt(document.getElementById('dayEnd').value);
+            var id;
 
-function AddTask() {
-    var root = document.getElementById('taskboard');
-    var begin = parseInt(document.getElementById('dayBegin').value);
-    var end = parseInt(document.getElementById('dayEnd').value);
-    var id;
-    var div = document.createElement('div');
-    div.className = 'task';
-    //div.innerHTML = 'Task: ' + taskDescr.value;
-    taskInner(div);
-   // closeBut.
-    for (var i=begin; i<=end;i++){
-        id = document.getElementById('day' + i);
-       id.style.backgroundColor='#CFB04C';
+            div.className = 'task';
+
+            //div.innerHTML = 'Task: ' + taskDescr.value;
+            taskHeader(div);
+            // closeBut.
+            for (var i = begin; i <= end; i++) {
+                id = document.getElementById('day' + i);
+                id.style.backgroundColor = '#CFB04C';
+            }
+            root.appendChild(div);
+        },
+        selectCategory: function () {
+            var selected = categorySelector.selectedIndex;
+
+            console.log(selected);
+            switch (selected) {
+                case 1: div.style.backgroundColor = '#8A2BE2'; break;
+                case 2: div.style.backgroundColor = '#66CD00'; break;
+                case 3: div.style.backgroundColor = '#FFD700'; break;
+
+            }
+
+        }
     }
-    root.appendChild(div);
+}());
+
+
+
+
+function taskHeader(parent) {
+    var closeBut, categoryContainer, categories;
+    var headDiv;
+    headDiv = document.createElement('div');
+
+
+    categoryContainer = document.createElement('div');
+    categoryContainer.className = 'taskHeader';
+    categories = document.createElement('select');
+    categories.id = 'categorySelector';
+
+    categories.onchange = Task.selectCategory;
+    var none = document.createElement('option');
+    none.innerHTML = 'Select category';
+    categories.appendChild(none);
+    var cat1 = document.createElement('option');
+    cat1.innerHTML = 'Coding';
+    cat1.id = 'catOne';
+    categories.appendChild(cat1);
+    var cat2 = document.createElement('option');
+    cat2.innerHTML = 'Homework';
+    cat2.id = 'catTwo';
+    categories.appendChild(cat2);
+    var cat3 = document.createElement('option');
+    cat3.innerHTML = 'Other';
+    cat3.id = 'catThree';
+    categories.appendChild(cat3);
+
+    categoryContainer.appendChild(categories);
+    headDiv.appendChild(categoryContainer);
+    closeBut = document.createElement('img');
+    closeBut.src = 'images\\Close_delete_remove_exit_cross_x_button_error.png';
+    closeBut.alt = 'closeB';
+    closeBut.className = 'closeButton';
+    headDiv.appendChild(closeBut);
+    parent.appendChild(headDiv);
 }
 
+function taskBody(parent) {
 
-function taskInner( parent){
- var closeBut;
- var childDiv;
- childDiv = document.createElement('div');
-
- parent.appendChild(childDiv);
- closeBut = document.createElement('img');
- closeBut.src = 'images\\Close_delete_remove_exit_cross_x_button_error.png';
- closeBut.alt = 'closeB';
- closeBut.className = 'closeTask';
- childDiv.appendChild(closeBut);
 }
+
